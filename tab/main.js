61bb752f785ee40b574de12eeee4a6aa3b76b236
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2021-08-21 06:53:43 +0900

    tab

---
+++ tab/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+{
+    const tabLabels = document.querySelectorAll('.tab__label li a');
+    const tabContents = document.querySelectorAll('.tab__content');
+
+    console.log(tabLabels);
+
+    // forEach   for-of
+    tabLabels.forEach(clickedLabel => {
+        clickedLabel.addEventListener('click', e => {
+            console.log(clickedLabel);
+            e.preventDefault();
+
+            tabLabels.forEach(label => {
+                label.classList.remove('active');
+            });
+
+            clickedLabel.classList.add('active');
+
+            tabContents.forEach(content => {
+                content.classList.remove('active');
+            });
+
+            document.getElementById(clickedLabel.dataset.id).classList.add('active');
+        })
+    });
+}
